ce93936a554080bd2649d1fb738a91f2428818c7
Jiyong Sim <sjy19910222@gmail.com>
2022-09-06 12:52:34 +0900

    init : 초기값 설정

---
+++ src/main/java/com/nikitoe/bankfinancialinstruments/BankFinancialInstrumentsApplication.java
@@ -0,0 +1,13 @@
+package com.nikitoe.bankfinancialinstruments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankFinancialInstrumentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankFinancialInstrumentsApplication.class, args);
+	}
+
+}
